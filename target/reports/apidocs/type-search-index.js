b718fc240dd26a742dc28fb1af96759e02e0a376
Aritz <herreroaritz@gmail.com>
2024-10-14 10:13:34 +0200

    Añado el javadoc

---
+++ target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.aritzherrero.ejerciciob","l":"HelloApplication"},{"p":"es.aritzherrero.ejerciciob.Controladores","l":"PersonaController"}];updateSearchResults();
